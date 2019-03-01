d80a753910d99519599313b1d51446eec9ab9437
anaqui <aliaamir@gmail.com>
2019-03-01 09:42:51 -0500

    Added files for listing 2 items.

---
+++ app/src/main/java/com/voltaps/voltapowersystems/BleDevice.java
@@ -0,0 +1,44 @@
+package com.voltaps.voltapowersystems;
+
+public class BleDevice{
+    private String name;
+    private String address;
+    public final String[] serviceStrings = {
+            "bleDevice",
+            "remapCmd",
+            "LedCmd"};
+
+    public enum serviceStringsIndex {
+        BLEDEVICE,
+        CONNECTION_STATUS,
+        ACK_REMAP_CMD,
+        ACK_LED_CMD,
+        ACK_REFLASH_CMD,
+        REMAP_CMD,
+        LED_CMD,
+        REFLASH_CMD
+
+    }
+
+    public BleDevice(String name, String address) {
+        super();
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ app/src/main/java/com/voltaps/voltapowersystems/TwoItemListAdapter.java
@@ -0,0 +1,90 @@
+package com.voltaps.voltapowersystems;
+
+import android.widget.BaseAdapter;
+import android.content.Context;
+import java.util.ArrayList;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+/**
+ * Description:
+ * Class to display 2 item list.
+ */
+public class TwoItemListAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layoutResourceID;
+    private ArrayList<BleDevice> devices;
+
+    TwoItemListAdapter(Context context, int resource, ArrayList<BleDevice> devices) {
+        this.context = context;
+        this.devices = devices;
+        layoutResourceID = resource;
+    }
+
+    /**
+     * Description:
+     * Returns number of list items.
+     * @return - returns number of listed items.
+     */
+    @Override
+    public int getCount() {
+        return devices.size();
+    }
+
+    /**
+     * Description:
+     * Get an item in the list.
+     * @param position
+     * position in list.
+     * @return - Device object in list.
+     */
+    @Override
+    public Object getItem(int position) {
+        return devices.get(position);
+    }
+
+    /**
+     * Description:
+     * Get item ID. Not implemented, do NOT use.
+     * @param position - Position in list.
+     * @return - 0.
+     */
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    /**
+     * Description:
+     * View to define how to display the data.
+     * @param position - Position in list.
+     * @param convertedView - View the list will be displayed in.
+     * @param parent - Parent ViewGroup.
+     * @return - View
+     */
+    @Override
+    public View getView(int position, View convertedView, ViewGroup parent) {
+        // Make sure we have a view to use
+        View itemView = convertedView;
+        if (itemView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            itemView = inflater.inflate(layoutResourceID, parent, false);
+        }
+
+        // Find the device
+        BleDevice currentDevice = devices.get(position);
+
+        // Fill the View (show the data from the BleDevice class)
+        TextView dNameText = itemView.findViewById(R.id.name);
+        dNameText.setText(currentDevice.getName());
+
+        TextView dAddressText = itemView.findViewById(R.id.address);
+        dAddressText.setText(currentDevice.getAddress());
+
+        // Return the View
+        return itemView;
+    }
+}
