fd6d4a3d63764b2da77d720f738b4dcdee5444e1
anaqui <aliaamir@gmail.com>
2019-03-10 21:29:01 -0400

    Added 2 graphs and modified to show dummy battery data.

--- app/src/main/java/com/voltaps/voltapowersystems/PerformanceFragment.java
+++ app/src/main/java/com/voltaps/voltapowersystems/PerformanceFragment.java
@@ -99,26 +99,11 @@ public class PerformanceFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        PieChart();
-        LineGraph();
+        LineGraph2();
+        LineGraph1();
     }
 
-    private void PieChart(){
-        Pie pie = AnyChart.pie();
-
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("John", 10000));
-        data.add(new ValueDataEntry("Jake", 12000));
-        data.add(new ValueDataEntry("Peter", 18000));
-
-        pie.data(data);
-
-        AnyChartView anyChartView1 = (AnyChartView) getView().findViewById(R.id.any_chart_view1);
-        APIlib.getInstance().setActiveAnyChartView(anyChartView1);
-        anyChartView1.setChart(pie);
-    }
-
-    private void LineGraph() {
+    private void LineGraph2(){
         AnyChartView anyChartView2 = getView().findViewById(R.id.any_chart_view2);
         APIlib.getInstance().setActiveAnyChartView(anyChartView2);
         //anyChartView.setProgressBar(getView().findViewById(R.id.progress_bar));
@@ -137,36 +122,37 @@ public class PerformanceFragment extends Fragment {
 
         cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
 
-        cartesian.title("Trend of Sales of the Most Popular Products of ACME Corp.");
+        cartesian.title("Temperature Change Over Time");
 
-        cartesian.yAxis(0).title("Number of Bottles Sold (thousands)");
+        cartesian.yAxis(0).title("Temperature (F)");
+        cartesian.xAxis(0).title("Time (Minutes)");
         cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
 
         List<DataEntry> seriesData = new ArrayList<>();
-        seriesData.add(new CustomDataEntry("1986", 3.6, 2.3, 2.8));
-        seriesData.add(new CustomDataEntry("1987", 7.1, 4.0, 4.1));
-        seriesData.add(new CustomDataEntry("1988", 8.5, 6.2, 5.1));
-        seriesData.add(new CustomDataEntry("1989", 9.2, 11.8, 6.5));
-        seriesData.add(new CustomDataEntry("1990", 10.1, 13.0, 12.5));
-        seriesData.add(new CustomDataEntry("1991", 11.6, 13.9, 18.0));
-        seriesData.add(new CustomDataEntry("1992", 16.4, 18.0, 21.0));
-        seriesData.add(new CustomDataEntry("1993", 18.0, 23.3, 20.3));
-        seriesData.add(new CustomDataEntry("1994", 13.2, 24.7, 19.2));
-        seriesData.add(new CustomDataEntry("1995", 12.0, 18.0, 14.4));
-        seriesData.add(new CustomDataEntry("1996", 3.2, 15.1, 9.2));
-        seriesData.add(new CustomDataEntry("1997", 4.1, 11.3, 5.9));
-        seriesData.add(new CustomDataEntry("1998", 6.3, 14.2, 5.2));
-        seriesData.add(new CustomDataEntry("1999", 9.4, 13.7, 4.7));
-        seriesData.add(new CustomDataEntry("2000", 11.5, 9.9, 4.2));
-        seriesData.add(new CustomDataEntry("2001", 13.5, 12.1, 1.2));
-        seriesData.add(new CustomDataEntry("2002", 14.8, 13.5, 5.4));
-        seriesData.add(new CustomDataEntry("2003", 16.6, 15.1, 6.3));
-        seriesData.add(new CustomDataEntry("2004", 18.1, 17.9, 8.9));
-        seriesData.add(new CustomDataEntry("2005", 17.0, 18.9, 10.1));
-        seriesData.add(new CustomDataEntry("2006", 16.6, 20.3, 11.5));
-        seriesData.add(new CustomDataEntry("2007", 14.1, 20.7, 12.2));
-        seriesData.add(new CustomDataEntry("2008", 15.7, 21.6, 10));
-        seriesData.add(new CustomDataEntry("2009", 12.0, 22.5, 8.9));
+        seriesData.add(new CustomSingleDataEntry("230", 20));
+        seriesData.add(new CustomSingleDataEntry("220", 21));
+        seriesData.add(new CustomSingleDataEntry("210", 22));
+        seriesData.add(new CustomSingleDataEntry("200", 23));
+        seriesData.add(new CustomSingleDataEntry("190", 24));
+        seriesData.add(new CustomSingleDataEntry("180", 25));
+        seriesData.add(new CustomSingleDataEntry("170", 26));
+        seriesData.add(new CustomSingleDataEntry("160", 27));
+        seriesData.add(new CustomSingleDataEntry("150",  30));
+        seriesData.add(new CustomSingleDataEntry("140", 32));
+        seriesData.add(new CustomSingleDataEntry("130", 33));
+        seriesData.add(new CustomSingleDataEntry("120", 34));
+        seriesData.add(new CustomSingleDataEntry("110", 35));
+        seriesData.add(new CustomSingleDataEntry("100", 36));
+        seriesData.add(new CustomSingleDataEntry("90", 36));
+        seriesData.add(new CustomSingleDataEntry("80", 36));
+        seriesData.add(new CustomSingleDataEntry("70", 37));
+        seriesData.add(new CustomSingleDataEntry("60", 37));
+        seriesData.add(new CustomSingleDataEntry("50", 35));
+        seriesData.add(new CustomSingleDataEntry("40", 34));
+        seriesData.add(new CustomSingleDataEntry("30", 33));
+        seriesData.add(new CustomSingleDataEntry("20", 33));
+        seriesData.add(new CustomSingleDataEntry("10", 32));
+        seriesData.add(new CustomSingleDataEntry("Present", 31));
 
         Set set = Set.instantiate();
         set.data(seriesData);
@@ -175,7 +161,7 @@ public class PerformanceFragment extends Fragment {
         Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
 
         Line series1 = cartesian.line(series1Mapping);
-        series1.name("Brandy");
+        series1.name("Temp");
         series1.hovered().markers().enabled(true);
         series1.hovered().markers()
                 .type(MarkerType.CIRCLE)
@@ -186,7 +172,7 @@ public class PerformanceFragment extends Fragment {
                 .offsetX(5d)
                 .offsetY(5d);
 
-        Line series2 = cartesian.line(series2Mapping);
+       /* Line series2 = cartesian.line(series2Mapping);
         series2.name("Whiskey");
         series2.hovered().markers().enabled(true);
         series2.hovered().markers()
@@ -208,9 +194,115 @@ public class PerformanceFragment extends Fragment {
                 .position("right")
                 .anchor(Anchor.LEFT_CENTER)
                 .offsetX(5d)
+                .offsetY(5d);*/
+
+        cartesian.legend().enabled(false);
+        cartesian.legend().fontSize(13d);
+        cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+        anyChartView2.setChart(cartesian);
+    }
+
+    private void LineGraph1() {
+        AnyChartView anyChartView2 = getView().findViewById(R.id.any_chart_view1);
+        APIlib.getInstance().setActiveAnyChartView(anyChartView2);
+        //anyChartView.setProgressBar(getView().findViewById(R.id.progress_bar));
+
+        Cartesian cartesian = AnyChart.line();
+
+        cartesian.animation(true);
+
+        cartesian.padding(10d, 20d, 5d, 20d);
+
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                // TODO ystroke
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("State of Charge Over Time");
+
+        // Y-axis max and min scaling
+        cartesian.yScale().minimum(0);
+        cartesian.yScale().maximum(100);
+
+        cartesian.yGrid(0, true);
+
+        cartesian.yAxis(0).title("State of Charge (%)");
+        cartesian.xAxis(0).title("Time (Minutes)");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+        List<DataEntry> seriesData = new ArrayList<>();
+        seriesData.add(new CustomSingleDataEntry("230", 95));
+        seriesData.add(new CustomSingleDataEntry("220", 90));
+        seriesData.add(new CustomSingleDataEntry("210", 85));
+        seriesData.add(new CustomSingleDataEntry("200", 75));
+        seriesData.add(new CustomSingleDataEntry("190", 65));
+        seriesData.add(new CustomSingleDataEntry("180", 55));
+        seriesData.add(new CustomSingleDataEntry("170", 60));
+        seriesData.add(new CustomSingleDataEntry("160", 62));
+        seriesData.add(new CustomSingleDataEntry("150",  45));
+        seriesData.add(new CustomSingleDataEntry("140", 35));
+        seriesData.add(new CustomSingleDataEntry("130", 45));
+        seriesData.add(new CustomSingleDataEntry("120", 47));
+        seriesData.add(new CustomSingleDataEntry("110", 53));
+        seriesData.add(new CustomSingleDataEntry("100", 57));
+        seriesData.add(new CustomSingleDataEntry("90", 60));
+        seriesData.add(new CustomSingleDataEntry("80", 62));
+        seriesData.add(new CustomSingleDataEntry("70", 65));
+        seriesData.add(new CustomSingleDataEntry("60", 55));
+        seriesData.add(new CustomSingleDataEntry("50", 47));
+        seriesData.add(new CustomSingleDataEntry("40", 53));
+        seriesData.add(new CustomSingleDataEntry("30", 58));
+        seriesData.add(new CustomSingleDataEntry("20", 65));
+        seriesData.add(new CustomSingleDataEntry("10", 70));
+        seriesData.add(new CustomSingleDataEntry("Present", 75));
+
+        Set set = Set.instantiate();
+        set.data(seriesData);
+        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        //Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+        //Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
+
+        Line series1 = cartesian.line(series1Mapping);
+        series1.name("SOC");
+        series1.hovered().markers().enabled(true);
+        series1.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series1.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
                 .offsetY(5d);
 
-        cartesian.legend().enabled(true);
+       /* Line series2 = cartesian.line(series2Mapping);
+        series2.name("Whiskey");
+        series2.hovered().markers().enabled(true);
+        series2.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series2.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        Line series3 = cartesian.line(series3Mapping);
+        series3.name("Tequila");
+        series3.hovered().markers().enabled(true);
+        series3.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series3.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);*/
+
+        cartesian.legend().enabled(false);
         cartesian.legend().fontSize(13d);
         cartesian.legend().padding(0d, 0d, 10d, 0d);
 
@@ -235,7 +327,13 @@ public class PerformanceFragment extends Fragment {
             setValue("value2", value2);
             setValue("value3", value3);
         }
+    }
 
+    private class CustomSingleDataEntry extends ValueDataEntry {
+
+        CustomSingleDataEntry(String x, Number value) {
+            super(x, value);
+        }
     }
 
     @Override
